f913b9934d0380366bbc91ed3cc5aded676a1ee0
HPDipendra <45689181+HPDipendra@users.noreply.github.com>
2023-07-15 17:08:04 +0545

    changes : mysql database connection initialized

---
+++ src/config/database.config.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import env from "./env";
+
+const appDataSource = new DataSource({
+    type: "mysql",
+    host: env.DATABASE_HOST,
+    port: env.DATABASE_PORT,
+    username: env.DATABASE_USERNAME,
+    password: env.DATABASE_PASSWORD,
+    database: env.DATABASE_NAME,
+    // synchronize: true ,
+    logging: true,
+    // dropSchema: true,
+    entities: ["./src/entities/**/*entity.ts"],
+    subscribers: [],
+    migrations: [],
+});
+
+export default appDataSource;
---
+++ src/config/env.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+class env {
+    static NODE_ENV = process.env.NODE_ENVIRONMENT;
+    static PORT = +process.env.PORT!;
+
+    // *Database Configurations
+    static DATABASE_HOST = process.env.DATABASE_HOST;
+    static DATABASE_PORT = +process.env.DATABASE_PORT!;
+    static DATABASE_USERNAME = process.env.DATABASE_USERNAME;
+    static DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+    static DATABASE_NAME = process.env.DATABASE_NAME;
+
+    // *Other Configurations
+    static DEBUG_MODE = process.env.DEBUG_MODE;
+    static JWT_SECRET = process.env.JWT_SECRET!;
+    static TOKEN_EXPIRES_IN = process.env.TOKEN_EXPIRES_IN!;
+
+    // *Server Information
+    static BASE_URL = process.env.BASE_URL!;
+}
+
+export default env;
