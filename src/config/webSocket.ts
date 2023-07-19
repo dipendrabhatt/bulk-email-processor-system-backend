2f0fe32c1de25c69299ae28dc8be020e5e78e810
HPDipendra <45689181+HPDipendra@users.noreply.github.com>
2023-07-19 10:38:57 +0545

    changes : refactor code

--- src/config/webSocket.ts
+++ src/config/webSocket.ts
@@ -27,18 +27,14 @@ class WebSocketServer {
     }
     private initializeAuthentication() {
         this.server.use((socket, next) => {
-            console.log("inside authentication")
             const token = socket.handshake.auth.token;
-            console.log("🚀 ~ file: webSocket.ts:32 ~ WebSocketServer ~ this.server.use ~ token:", token)
             if (!token) {
                 return next(new Error("Authentication error"));
             }
             jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, decoded: any) => {
                 if (err) {
-                    console.log(err);
                     return next(new Error('Authentication error: invalid token'));
                 }
-                console.log(decoded, "Decoded data in middleware")
                 next();
             });
         });
